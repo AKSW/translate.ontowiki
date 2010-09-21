9def9892bc62447aabf616e14ec142b09577998b
Tim Ermilov <yamalight@gmail.com>
2010-09-21 15:04:52 +0200

    translation component properties select

--- ModeltranslateController.php
+++ ModeltranslateController.php
@@ -2,6 +2,8 @@
 
 require_once 'OntoWiki/Controller/Component.php';
 require_once 'OntoWiki/Toolbar.php';
+require_once 'OntoWiki/Navigation.php';
+require_once 'Erfurt/Sparql/Query2.php';
 
 /**
  * Controller for OntoWiki Filter Module
@@ -12,23 +14,37 @@ require_once 'OntoWiki/Toolbar.php';
  * @copyright  Copyright (c) 2009, {@link http://aksw.org AKSW}
  * @license    http://opensource.org/licenses/gpl-license.php GNU General Public License (GPL)
  */
+
 class ModeltranslateController extends OntoWiki_Controller_Component {
 
     public function init() {
-
         parent::init();
-        $this->owApp = OntoWiki::getInstance(); 
+        $this->_owApp = OntoWiki::getInstance();
+        $this->store = $this->_owApp->erfurt->getStore();
+        $this->translate = $this->_owApp->translate;
+        $this->ac = $this->_erfurt->getAc();
+        $this->locale       = $this->_owApp->config->languages->locale;
+        $this->titleHelper  = new OntoWiki_Model_TitleHelper($this->_owApp->selectedModel);
+        $this->view->locale      = $this->locale;
+        $this->view->titleHelper = $this->titleHelper;
 
         // disable tabs
-        require_once 'OntoWiki/Navigation.php';
         OntoWiki_Navigation::disableNavigation();
-
-        // get translation object
-        $this->translate = $this->_owApp->translate;
 
         //set title of main window ...
         $this->view->placeholder('main.window.title')->set($this->translate->_('Model Translation', $this->_config->languages->locale));
-
+        
+        // get selected Model
+        $this->model = $this->_owApp->selectedModel;
+        // check if no model selected
+        if (empty($this->model)) {
+            throw new OntoWiki_Exception('Missing parameter m (model) and no selected model in session!');
+            exit;
+        }
+        // check Model Based Access Control
+        if (!$this->ac->isModelAllowed('view', $this->model->getModelIri()) ) {
+            throw new Erfurt_Ac_Exception('You are not allowed to read this model.');
+        }
     }
 
     /**
@@ -36,34 +52,27 @@ class ModeltranslateController extends OntoWiki_Controller_Component {
      *
      */
     public function initAction() {
-
-        //DISPATCHING SOME ACTIONS
-        if (isset($this->_request->actionType)) {
-
-            if ( $this->_request->actionType == "viewTriple" ) {
-                $limit      = $this->_request->getParam('limit', null, false);
-                $occurence  = $this->_request->getParam('occurence', null, false);
-
-                $forms['showTriple']['limit']       = $limit;
-                $forms['showTriple']['occurence']   = $occurence;
-
-            }
-
-            if ( $this->_request->actionType == "createViews" ) {
-
-                $limit      = $this->_request->getParam('limit', null, false);
-                $occurence  = $this->_request->getParam('occurence', null, false);
-
-                $forms['createViews']['limit']      = $limit;
-                $forms['createViews']['occurence']  = $occurence;
-
-            }
-
+        $subjectVar = new Erfurt_Sparql_Query2_Var('subject');
+        $predicateVar = new Erfurt_Sparql_Query2_Var('predicate');
+        $objectVar = new Erfurt_Sparql_Query2_Var('object');
+        
+        $query = new Erfurt_Sparql_Query2();
+        $query->addProjectionVar($predicateVar)->setDistinct(true);
+        
+        $elements[] = new Erfurt_Sparql_Query2_Triple($subjectVar,$predicateVar,$objectVar);
+        $elements[] = new Erfurt_Sparql_Query2_Filter(new Erfurt_Sparql_Query2_isLiteral($objectVar));
+        
+        $query->addElements($elements);
+        
+        $query->setLimit(50);
+        
+        $predicates = $this->model->sparqlQuery($query);
+        
+        $this->view->predicates = array();
+        foreach ($predicates as $key => $predicate)
+        {
+            $this->titleHelper->addResource($predicate['predicate']);
+            $this->view->predicates[] = $predicate['predicate'];
         }
-        $url    = new OntoWiki_Url(array('controller' => 'modeltranslate', 'action' => 'init'));
-        $forms['showTriple']['action']  = (string) $url;
-        $forms['createViews']['action'] = (string) $url;
-        $this->view->forms = $forms;
     }
 }
-
